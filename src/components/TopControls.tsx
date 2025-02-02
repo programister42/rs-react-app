56f5edcd775a072cbf0d410b917cd4ed70143b61
Taras Tereschenko <tarrrasio@gmail.com>
2025-02-03 04:27:42 +0500

    fix: fix TopControls

--- src/components/TopControls.tsx
+++ src/components/TopControls.tsx
@@ -35,7 +35,7 @@ export class TopControls extends Component<TopControlsProps, TopControlsState> {
           type="text"
           placeholder="Search..."
           autoComplete="search"
-          className="block grow rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+          className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
           value={this.state.search}
           onChange={this.handleSearchInputChange}
           onKeyUp={(e) => e.key === 'Enter' && this.handleSearch()}
