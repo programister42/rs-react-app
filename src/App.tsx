0c0d186190d82ddbffdd88b49f9a3291ea28fe6f
Taras Tereschenko <tarrrasio@gmail.com>
2025-02-09 23:27:59 +0500

    feat: refactor App component to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { Component } from 'react';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import { SearchPage } from './pages/SearchPage';
 
-class App extends Component {
-  render() {
-    return (
-      <ErrorBoundary>
-        <SearchPage />
-      </ErrorBoundary>
-    );
-  }
+export default function App() {
+  return (
+    <ErrorBoundary>
+      <SearchPage />
+    </ErrorBoundary>
+  );
 }
-
-export default App;
