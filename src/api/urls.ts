941943ed7817c42ba802ab638576173cba95849e
Taras Tereschenko <tarrrasio@gmail.com>
2025-02-10 19:49:01 +0500

    refactor: refactor deployment process

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -1,3 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL ?? '/';
+export const BASE_URL: string =
+  (typeof process !== 'undefined' && process.env.BASE_URL) || '/';
 
 export const OPEN_LIBRARY_URL = 'https://openlibrary.org';
