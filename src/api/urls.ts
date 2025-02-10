13885243aa968881af1729381f065b6af9f99761
Taras Tereschenko <tarrrasio@gmail.com>
2025-02-10 19:20:27 +0500

    refactor: refactor deployment process

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -1,4 +1,1 @@
-export const BASE_PATH: string =
-  (typeof process !== 'undefined' && process.env.BASE_PATH) || '/kek/';
-
 export const OPEN_LIBRARY_URL = 'https://openlibrary.org';
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { BASE_PATH } from './src/api/urls';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: BASE_PATH,
   plugins: [react(), tailwindcss()],
 });
