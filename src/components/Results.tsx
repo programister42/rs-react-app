10b979ab100ead59a6b23fd4b3ef8de0b5acc049
Taras Tereschenko <tarrrasio@gmail.com>
2025-02-10 21:56:49 +0500

    refactor: migrate Results to functional component

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useContext } from 'react';
 import { BookSearchResponse } from '../api/models';
 import { ErrorContext } from './ErrorBoundary';
 
@@ -7,53 +7,48 @@ interface ResultsProps {
   isLoading: boolean;
 }
 
-export class Results extends Component<ResultsProps> {
-  static contextType = ErrorContext;
-  declare context: React.ContextType<typeof ErrorContext>;
+export function Results({ response, isLoading }: ResultsProps) {
+  const { error } = useContext(ErrorContext);
 
-  render() {
-    const { isLoading, response } = this.props;
+  const Error = error && (
+    <h2 className="text-base/7 font-semibold text-red-600">
+      ❌ {error.message}
+    </h2>
+  );
 
-    const error = this.context?.error && (
-      <h2 className="text-base/7 font-semibold text-red-600">
-        ❌ {this.context.error.message}
-      </h2>
-    );
+  const Loader = isLoading && (
+    <h2 className="animate-pulse text-base/7 font-semibold">Loading...</h2>
+  );
 
-    const loader = isLoading && (
-      <h2 className="animate-pulse text-base/7 font-semibold">Loading...</h2>
-    );
+  const Books =
+    response?.docs.length &&
+    response.docs.map((book, i, arr) => {
+      const booksLength = arr.length;
+      const isFirst = i === 0;
+      const isLast = booksLength && i === booksLength - 1;
+      return (
+        <li
+          key={book.key}
+          className={`flex flex-col py-4${isFirst ? ' pt-0' : ''}${isLast ? ' pb-0' : ''}${!isLast ? ' border-b border-gray-200' : ''}`}
+        >
+          <h3 className="text-lg font-medium tracking-tight text-gray-950">
+            📖&nbsp;{book.title}
+          </h3>
+          <p className="text-sm/6 text-gray-600 ">
+            {book.author_name.length > 1 ? '👥' : '👤'}&nbsp;
+            {book.author_name.join(', ')}
+          </p>
+        </li>
+      );
+    });
 
-    const books =
-      response?.docs.length &&
-      response.docs.map((book, i, arr) => {
-        const booksLength = arr.length;
-        const isFirst = i === 0;
-        const isLast = booksLength && i === booksLength - 1;
-        return (
-          <li
-            key={book.key}
-            className={`flex flex-col py-4${isFirst ? ' pt-0' : ''}${isLast ? ' pb-0' : ''}${!isLast ? ' border-b border-gray-200' : ''}`}
-          >
-            <h3 className="text-lg font-medium tracking-tight text-gray-950">
-              📖&nbsp;{book.title}
-            </h3>
-            <p className="text-sm/6 text-gray-600 ">
-              {book.author_name.length > 1 ? '👥' : '👤'}&nbsp;
-              {book.author_name.join(', ')}
-            </p>
-          </li>
-        );
-      });
+  const NoContent = (
+    <h2 className="text-base/7 font-semibold">No content 🤷</h2>
+  );
 
-    const noContent = (
-      <h2 className="text-base/7 font-semibold">No content 🤷</h2>
-    );
-
-    return (
-      <ul className="inset-px flex h-full flex-col overflow-hidden rounded-lg bg-white p-8 ring-1 shadow-sm ring-black/5 sm:p-10">
-        {error || loader || books || noContent}
-      </ul>
-    );
-  }
+  return (
+    <ul className="inset-px flex h-full flex-col overflow-hidden rounded-lg bg-white p-8 ring-1 shadow-sm ring-black/5 sm:p-10">
+      {Error || Loader || Books || NoContent}
+    </ul>
+  );
 }
