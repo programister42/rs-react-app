f1f4f9a9443a0ad31342ce4010677cf64c908079
Taras Tereschenko <tarrrasio@gmail.com>
2025-02-10 23:21:51 +0500

    feat: implement custom useLocalStorage hook

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,17 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, Dispatch<SetStateAction<T>>] {
+  const [value, setValue] = useState<T>(() => {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
